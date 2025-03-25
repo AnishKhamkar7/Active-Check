75cd3908ea6aaea2d85b3298312afdb81fff9e3f
Anish <anishkhamkar7@gmail.com>
2025-03-25 09:53:54 +0530

    feat: added typescript and prisma dependencies in package json files

---
+++ apps/api/src/index.ts
@@ -0,0 +1,13 @@
+
+import express, { Request, Response } from "express";
+const app = express()
+
+app.post('/',(req:Request,res:Response)=>{
+
+})
+
+app.get('/',(req:Request,res:Response)=>{
+    
+})
+
+app.listen(3000)
